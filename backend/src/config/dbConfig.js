b49a56268819972063360cd8a832bb482f0cc810
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-02-07 12:16:05 -0300

    feat(config): adiciona arquivo de conexão com o banco de dados

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// CONEXÃO COM O DB MONGODB ATLAS
-const connection = mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// CONEXÃO COM O BANCO DE DADOS MONGODB ATLAS
+mongoose.set('strictQuery', true);
+const connection = mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error(`Error to connect: ${err}`));
 
 module.exports = connection;
