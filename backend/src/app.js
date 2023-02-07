2ca31d8e5590377aa02a1441bf3bb58313d91d42
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-02-07 12:32:48 -0300

    feat(app.js): adiciona arquivo raiz da aplicação do backend Este commit inclui o arquivo responsável por fazer a conexão com toda a estrutura do backend e inicializar a aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const admin = require('./routes/admin');
 
 require('./config/dbConfig');
+require('./models/Post');
+require('./models/Category');
+
+// const Post = mongoose.model('posts');
+// const Category = mongoose.model('categories');
+
+const admin = require('./routes/admin');
 
 const app = express();
 
 app.use(express.json());
+app.use(admin);
 
 // CONFIGURAÇÕES
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// ROTAS
-app.get('/', (req, res) => {
-  return res.json({
-    nome: 'Gustavo',
-    profissao: 'Desenvolvedor',
-  });
-});
-
-app.use('/admin', admin);
-
 // HTTP SERVER
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT} => http://localhost:${PORT}`);
