60382c0e145dee555e084b0bf56f91f0f51109c4
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-02-07 14:34:13 -0300

    feat(routes): adiciona rotas administrativas de Posts Este commit inclui as rotas de CRUD de acesso administrativo de Posts

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const CategoryController = require('../controllers/CategoryController');
+const PostController = require('../controllers/PostController');
 
+// Rota Category
 router.get('/admin/categories', CategoryController.read);
 router.post('/admin/categories', CategoryController.create);
-router.delete('/admin/categories/:id', CategoryController.delete);
 router.put('/admin/categories/:id', CategoryController.update);
+router.delete('/admin/categories/:id', CategoryController.delete);
+
+// Rota Post
+router.get('/admin/posts', PostController.read);
+router.post('/admin/posts', PostController.create);
+router.put('/admin/posts/:id', PostController.update);
+router.delete('/admin/posts/:id', PostController.delete);
 
 module.exports = router;
