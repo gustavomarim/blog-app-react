1ef7515c3fbd41afc6c81d7d5e492a437c232597
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-02-11 15:41:51 -0300

    perf(NavBar): melhoria no desempenho de roteamento de links Este commit realiza a alteração do roteamento de links que era feito através do href padrão do HTML, onde sua ação gerava um recarregamento de página. Agora, com o Link do react-router-dom, há uma melhora no desempenho da aplicação, por sua ação gerar apenas a renderização do componente solicitado, seguindo as práticas de uma SPA (Single Page Application).

--- frontend/src/components/template/NavBar.tsx
+++ frontend/src/components/template/NavBar.tsx
@@ -1,17 +1,30 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
     <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
       <Container>
-        <Navbar.Brand href='/'>Blog App</Navbar.Brand>
+        <Link to='/' className='text-decoration-none'>
+          <Navbar.Brand>Blog App</Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav className='me-auto'>
-            <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+            <Link to='/' className='text-decoration-none nav-link'>
+              Home
+            </Link>
+            <Link to='/' className='text-decoration-none nav-link'>
+              Categorias
+            </Link>
+            <Link to='/' className='text-decoration-none nav-link'>
+              Login
+            </Link>
+            <Link to='/' className='text-decoration-none nav-link'>
+              Registro
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
