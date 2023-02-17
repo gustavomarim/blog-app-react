432f67c108ecec52269b29ee9254e380cd17f7e7
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-02-17 14:40:40 -0300

    feat(frontend/core): adiciona funcionalidade de validação de usuário

--- frontend/src/core/users/User.ts
+++ frontend/src/core/users/User.ts
@@ -1,8 +1,17 @@
+import _ from '../../functions/_';
+import regex from '../../state/constants/regex';
+
 export interface UserProps {
-  id: string;
+  id?: string;
   name: string;
   email: string;
+  password: string;
 }
+
+type FieldName = 'name' | 'email' | 'password';
+type Field = {
+  [key: string]: () => boolean;
+};
 
 export default class User {
   private _props: UserProps;
@@ -12,4 +21,17 @@ export default class User {
   }
 
   // Funcionalidades, validações...
+  validate(fieldName: FieldName): boolean {
+    const field: Field = {
+      name: () => regex.onlyCharacter.test(this._props.name),
+      email: () => regex.email.test(this._props.email),
+      password: () => regex.password.test(this._props.password),
+    };
+
+    if (_.str.isString(fieldName)) {
+      return field[fieldName]();
+    }
+
+    return false;
+  }
 }
