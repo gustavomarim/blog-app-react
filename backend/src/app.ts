109be59a251fb4ab614b01aafd8aee4208c65929
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-10-01 19:21:54 -0300

    test: deploy vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -49,7 +49,7 @@ app.use(admin);
 app.use(post);
 app.use(category);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ?? 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT} => http://localhost:${PORT}`);
