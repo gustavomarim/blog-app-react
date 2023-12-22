f5501703a30440e00aa669215f043326146743fb
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-12-22 18:10:50 -0300

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { Container } from 'react-bootstrap';
-import './App.css';
-import { NavBar } from './components/NavBar';
-import RoutesComponent from './routes';
+import { Container } from "react-bootstrap";
+import { QueryClientProvider } from "react-query";
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { queryClient } from "./core/api/queryClient";
+import RoutesComponent from "./routes";
 
 function App() {
   return (
-    <div className='App'>
-      <NavBar />
-      <Container>
-        <RoutesComponent />
-      </Container>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <NavBar />
+        <Container>
+          <RoutesComponent />
+        </Container>
+      </div>
+    </QueryClientProvider>
   );
 }
 
