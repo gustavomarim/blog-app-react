e8f1d1efc66f2ce7b82ac5bdc7d00535c6ebf2bd
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-12-23 11:16:38 -0300

    feat: adiciona hooks de login e registro de usuário

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,24 @@
+import { useMutation } from "react-query";
+import api from "../core/api/ApiService";
+import { LoginProps } from "../types/login";
+
+export const useLogin = () => {
+  const loginMutation = useMutation((dataForm: LoginProps) => {
+    const { email, password } = dataForm;
+
+    return api.post("/users/login", { email, password });
+  });
+
+  const handleLogin = async (values: LoginProps) => {
+    try {
+      await loginMutation.mutateAsync(values);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return {
+    login: handleLogin,
+    loginMutation,
+  };
+};
---
+++ src/hooks/useRegister.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useMutation } from "react-query";
+import api from "../core/api/ApiService";
+import { UserRegisterProps } from "../types/userRegister";
+
+export const useRegister = () => {
+  const [user, setUser] = useState(null);
+
+  const registerMutation = useMutation((dataForm: UserRegisterProps) => {
+    const { name, email, password, confirmPassword } = dataForm;
+
+    return api.post("/users/register", {
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+  });
+
+
+
+  const handleRegister = async (values: UserRegisterProps) => {
+    try {
+      await registerMutation.mutateAsync(values);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return {
+    user,
+    register: handleRegister,
+    registerMutation,
+  };
+};
