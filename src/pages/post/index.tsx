1a1cf57963d07eff6b341d38225249a69d831bbd
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-12-27 11:27:55 -0300

    refactor: separa lógica de componente com custom hook

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,39 +1,44 @@
-import { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
-import api from '../../core/api/ApiService';
-import _ from '../../functions/_';
-import { BlogPostProps } from '../../types/blogPost';
+import { Card } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { SweetAlert } from "../../components/Alert";
+import _ from "../../functions/_";
+import { useBlogPost } from "../../hooks/usePost";
+import { TIME_TO_SHOW_ALERT } from "../../state/constants/timeToShowAlert";
 
 export const Post = () => {
-  const [post, setPost] = useState<BlogPostProps>();
   const params = useParams();
   const slug = params.slug;
+  const { data, error, isLoading } = useBlogPost(`/posts/${slug}`);
+  const formattedDate = data ? _.fd.formatDate(data?.date) : "";
 
-  async function getPost(slug: string) {
-    const response = await api.get(`/posts/${slug}`);
-    const data = response.data;
-    setPost(data);
+  if (isLoading) <p>Carregando...</p>;
+
+  if (error) {
+    console.error(`Erro ao carregar postagens: ${error}`);
+    return (
+      <SweetAlert
+        message={`Erro ao carregar postagens::: ${error}`}
+        timeInMS={TIME_TO_SHOW_ALERT}
+        variant="warning"
+      />
+    );
   }
 
-  useEffect(() => {
-    if (typeof slug === 'string') getPost(slug);
-  }, []);
-
-  if (post)
-    return (
-      <Card>
-        <Card.Body>
-          <h1>{post.title}</h1>
-          <hr />
-          <small className='d-block'>
-            {`Data da publicação: ${_.fd.formatDate(post.date)}`}
-          </small>
-          <hr />
-          <Card.Text>{post.content}</Card.Text>
-        </Card.Body>
-      </Card>
-    );
-
-  return null;
+  return (
+    <>
+      {data && (
+        <Card>
+          <Card.Body>
+            <h1>{data.title}</h1>
+            <hr />
+            <small className="d-block">
+              {`Data da publicação: ${formattedDate}`}
+            </small>
+            <hr />
+            <Card.Text>{data.content}</Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+    </>
+  );
 };
