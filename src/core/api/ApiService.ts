6d1e6b8b827d18d1e797204724c439ec452d6c6f
gustavomarim <gustavo.dantas.marim@outlook.com>
2024-01-12 21:25:19 -0300

    feat(core): adicionar compartilhamento de cookies padrão no service api

--- src/core/api/ApiService.ts
+++ src/core/api/ApiService.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}`,
+  withCredentials: true,
 });
 
 export default api;
