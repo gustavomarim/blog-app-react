a8208e21ac4af3585f973ab84bdafe0cc2ee34b7
gustavomarim <gustavo.dantas.marim@outlook.com>
2024-01-25 00:50:44 -0300

    chore: atualiza dependencias das libs de testes unitários

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
+    testMatch: ["./tests/**/*.test.tsx"],
+    globals: true,
+    coverage: {
+      provider: "istanbul", // or 'v8'
+      reporter: ["text", "json", "html"],
+    },
+  },
+});
